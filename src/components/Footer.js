665ba581c457943d4b51242896e7e90c32c9db70
000Ren000 <antonren@yandex.ru>
2022-07-07 11:24:50 +0700

    психанул все снес чтоб начать сначала

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer () {
-	return (
-			<footer className="footer conteiner">
-				<p className="footer__text">&copy; {new Date().getFullYear()} Mesto Russia</p>
-			</footer>
-	);
-}
--- src/components/Header.js
+++
@@ -1,8 +0,0 @@
-import logo from '../images/logo.svg';
-export default function Header () {
-	return (
-			<header className="header">
-				<img className="header__logo" src={logo} alt="Логотип"/>
-			</header>
-	)
-}
