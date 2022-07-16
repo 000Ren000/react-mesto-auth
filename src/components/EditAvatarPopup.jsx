0843117881b4c18ca2c378a77eb9a6da38966802
000Ren000 <antonren@yandex.ru>
2022-07-16 17:10:42 +0700

    fix / Здесь также следует поправить форматирование

--- src/components/EditAvatarPopup.jsx
+++ src/components/EditAvatarPopup.jsx
@@ -2,7 +2,7 @@ import PopupWithForm from './PopupWithForm.jsx';
 import {useRef} from 'react';
 
 export default function EditAvatarPopup(props) {
-	const	{isOpen, onClose, onSubmit} = props;
+	const {isOpen, onClose, onSubmit} = props;
 	const avatarRef = useRef();
 
 	function handleSubmit(e) {
@@ -14,21 +14,12 @@ export default function EditAvatarPopup(props) {
 		onClose();
 		avatarRef.current.value = '';
 	}
+
 	return (
-			<PopupWithForm title="Обновить аватар" name='add-Form'
-			               buttonText="Сохранить"
-			               isOpen={isOpen}
-			               onClose={onClose}
-			               onSubmit={handleSubmit}
-			>
-				<input name="link"
-				       id="input-avatar"
-				       placeholder="Ссылка на картинку"
-				       type="url"
-				       className="edit-form__input edit-form__input_type_link"
-				       ref={avatarRef}
-				       required
-				/>
+			<PopupWithForm title="Обновить аватар" name='add-Form' buttonText="Сохранить"
+			               isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit}>
+				<input name="link" id="input-avatar" placeholder="Ссылка на картинку" type="url"
+				       className="edit-form__input edit-form__input_type_link" ref={avatarRef} required/>
 				<span className="popup__error" id="input-avatar-error"></span>
 			</PopupWithForm>
 	)
