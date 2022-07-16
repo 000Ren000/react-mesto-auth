0496a9234742a7af7bb863a7af4767f31a53cd11
000Ren000 <antonren@yandex.ru>
2022-07-16 17:59:45 +0700

    fix / Не соблюдается иерархия отступов, следует поправить форматирование

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,78 +13,78 @@ import {api} from '../utils/Api.js';
 
 export function HomePage() {
 
-const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false)
-const [isAddPlacePopupOpen, setAddPlacePopupOpen] = useState(false);
-const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
-const [selectedCard, setSelectedCard] = useState({name: '', link: ''});
+	const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false)
+	const [isAddPlacePopupOpen, setAddPlacePopupOpen] = useState(false);
+	const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
+	const [selectedCard, setSelectedCard] = useState({name: '', link: ''});
 
-const handleEditAvatarClick = () => setEditAvatarPopupOpen(true);
-const handleEditProfileClick = () => setIsEditProfilePopupOpen(true);
-const handleAddPlaceClick = () => setAddPlacePopupOpen(true);
-const closeAllPopups = () => {
-	setEditAvatarPopupOpen(false);
-	setIsEditProfilePopupOpen(false);
-	setAddPlacePopupOpen(false);
-	setSelectedCard({name: '', link: ''});
-}
+	const handleEditAvatarClick = () => setEditAvatarPopupOpen(true);
+	const handleEditProfileClick = () => setIsEditProfilePopupOpen(true);
+	const handleAddPlaceClick = () => setAddPlacePopupOpen(true);
+	const closeAllPopups = () => {
+		setEditAvatarPopupOpen(false);
+		setIsEditProfilePopupOpen(false);
+		setAddPlacePopupOpen(false);
+		setSelectedCard({name: '', link: ''});
+	}
 
 //Данные о пользователе
-const [currentUser, setCurrentUser] = useState({
-	myID: '',
-	about: '',
-	avatar: '',
-	cohort: '',
-	name: ''
-})
-useEffect(() => {
-	api.getUserInfo().then(setCurrentUser
-	).catch(err => console.log('что-то пошло не так', err));
-}, []); 	//Получение данных о пользователе
+	const [currentUser, setCurrentUser] = useState({
+		myID: '',
+		about: '',
+		avatar: '',
+		cohort: '',
+		name: ''
+	})
+	useEffect(() => {
+		api.getUserInfo().then(setCurrentUser
+		).catch(err => console.log('что-то пошло не так', err));
+	}, []); 	//Получение данных о пользователе
 
-const handleUpdateUser = (data) => {
-	api.setUserInfo(data).then(setCurrentUser)
-			.catch(err => console.log('что-то пошло не так', err));
-}
-const handleUpdateAvatar = (data) => {
-	api.changeAvatar(data.avatar).then(setCurrentUser)
-			.catch(err => console.log('что-то пошло не так', err));
-}
+	const handleUpdateUser = (data) => {
+		api.setUserInfo(data).then(setCurrentUser)
+				.catch(err => console.log('что-то пошло не так', err));
+	}
+	const handleUpdateAvatar = (data) => {
+		api.changeAvatar(data.avatar).then(setCurrentUser)
+				.catch(err => console.log('что-то пошло не так', err));
+	}
 
 //Получение массива с карточками
-const [cards, setCards] = useState([]);
-useEffect(() => {
-	api.getCardInfo().then(data => {
-		setCards(data.map(item => ({...item, key: item._id})
-		));
-	})
-			.catch(err => console.log('что-то пошло не так', err));
-}, []);
+	const [cards, setCards] = useState([]);
+	useEffect(() => {
+		api.getCardInfo().then(data => {
+			setCards(data.map(item => ({...item, key: item._id})
+			));
+		})
+				.catch(err => console.log('что-то пошло не так', err));
+	}, []);
 
 
-function handleCardLike(card) {
-	// Снова проверяем, есть ли уже лайк на этой карточке
-	const isLiked = card.likes.some(i => i._id === currentUser._id);
+	function handleCardLike(card) {
+		// Снова проверяем, есть ли уже лайк на этой карточке
+		const isLiked = card.likes.some(i => i._id === currentUser._id);
 
-	// Отправляем запрос в API и получаем обновлённые данные карточки
-	api.changeLikeCardStatus(card._id, isLiked).then((newCard) => {
-		setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
-	})
-			.catch(err => console.log('что-то пошло не так', err));
-}
+		// Отправляем запрос в API и получаем обновлённые данные карточки
+		api.changeLikeCardStatus(card._id, isLiked).then((newCard) => {
+			setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+		})
+				.catch(err => console.log('что-то пошло не так', err));
+	}
 
-function handleAddPlaceSubmit(data) {
-	api.setNewCardInfo(data).then(newCard => {
-		setCards([newCard, ...cards])
-	})
-			.catch(err => console.log('что-то пошло не так', err));
-}
+	function handleAddPlaceSubmit(data) {
+		api.setNewCardInfo(data).then(newCard => {
+			setCards([newCard, ...cards])
+		})
+				.catch(err => console.log('что-то пошло не так', err));
+	}
 
-function handleCardDelete(card) {
-	api.deleteCard(card._id).then(newCard => {
-		setCards((state) => state.filter(c => c._id !== card._id))
-	})
-			.catch(err => console.log('что-то пошло не так', err));
-}
+	function handleCardDelete(card) {
+		api.deleteCard(card._id).then(newCard => {
+			setCards((state) => state.filter(c => c._id !== card._id))
+		})
+				.catch(err => console.log('что-то пошло не так', err));
+	}
 
 
 	return (
