c2870b641b8c7d86f456958ac96c7edd66707ac8
000Ren000 <antonren@yandex.ru>
2022-08-30 14:25:52 +0700

    fix / поченил авторизацию

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -11,4 +11,5 @@ export const key = {
   'Content-Type': 'application/json'
 }
 
-export const linkApi = 'http://localhost:3000'
+// export const linkApi = 'http://localhost:3000'
+export const linkApi = 'https://api.mesto.antonren.ru';
